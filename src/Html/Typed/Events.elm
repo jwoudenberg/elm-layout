5c3930db0062c73402c36f468f250af3f145fa68
Jasper Woudenberg <mail@jasperwoudenberg.com>
2018-02-10 09:56:16 +0100

    Make custom event handlers safer to use
    Most programs use some amount of custom event handlers as it is
    impossible to provide all possible combinations. To give these handlers
    attr types as well, we now pass in a field-accessor function.

--- src/Html/Typed/Events.elm
+++ src/Html/Typed/Events.elm
@@ -7,27 +7,27 @@ import Json.Decode exposing (Decoder)
 
 onClick : msg -> Attribute { r | onClick : msg } msg
 onClick msg =
-    on "click" (Json.Decode.succeed msg)
+    on .onClick "click" (Json.Decode.succeed msg)
 
 
 onDoubleClick : msg -> Attribute { r | onDoubleClick : msg } msg
 onDoubleClick msg =
-    on "dblclick" (Json.Decode.succeed msg)
+    on .onDoubleClick "dblclick" (Json.Decode.succeed msg)
 
 
 onInput : (String -> msg) -> Attribute { r | onInput : msg } msg
 onInput tagger =
-    on "input" (Json.Decode.map tagger Html.Events.targetValue)
+    on .onInput "input" (Json.Decode.map tagger Html.Events.targetValue)
 
 
 onCheck : (Bool -> msg) -> Attribute { r | onCheck : msg } msg
 onCheck tagger =
-    on "change" (Json.Decode.map tagger Html.Events.targetChecked)
+    on .onCheck "change" (Json.Decode.map tagger Html.Events.targetChecked)
 
 
 onSubmit : msg -> Attribute { r | onSubmit : msg } msg
 onSubmit msg =
-    onWithOptions "submit" onSubmitOptions (Json.Decode.succeed msg)
+    onWithOptions .onSubmit "submit" onSubmitOptions (Json.Decode.succeed msg)
 
 
 onSubmitOptions : Html.Events.Options
@@ -37,49 +37,49 @@ onSubmitOptions =
 
 onBlur : msg -> Attribute { r | onBlur : msg } msg
 onBlur msg =
-    on "blur" (Json.Decode.succeed msg)
+    on .onBlur "blur" (Json.Decode.succeed msg)
 
 
 onFocus : msg -> Attribute { r | onFocus : msg } msg
 onFocus msg =
-    on "focus" (Json.Decode.succeed msg)
+    on .onFocus "focus" (Json.Decode.succeed msg)
 
 
 onMouseDown : msg -> Attribute { r | onMouseDown : msg } msg
 onMouseDown msg =
-    on "mousedown" (Json.Decode.succeed msg)
+    on .onMouseDown "mousedown" (Json.Decode.succeed msg)
 
 
 onMouseUp : msg -> Attribute { r | onMouseUp : msg } msg
 onMouseUp msg =
-    on "mouseup" (Json.Decode.succeed msg)
+    on .onMouseUp "mouseup" (Json.Decode.succeed msg)
 
 
 onMouseEnter : msg -> Attribute { r | onMouseEnter : msg } msg
 onMouseEnter msg =
-    on "mouseenter" (Json.Decode.succeed msg)
+    on .onMouseEnter "mouseenter" (Json.Decode.succeed msg)
 
 
 onMouseLeave : msg -> Attribute { r | onMouseLeave : msg } msg
 onMouseLeave msg =
-    on "mouseleave" (Json.Decode.succeed msg)
+    on .onMouseLeave "mouseleave" (Json.Decode.succeed msg)
 
 
 onMouseOver : msg -> Attribute { r | onMouseOver : msg } msg
 onMouseOver msg =
-    on "mouseover" (Json.Decode.succeed msg)
+    on .onMouseOver "mouseover" (Json.Decode.succeed msg)
 
 
 onMouseOut : msg -> Attribute { r | onMouseOut : msg } msg
 onMouseOut msg =
-    on "mouseout" (Json.Decode.succeed msg)
+    on .onMouseOut "mouseout" (Json.Decode.succeed msg)
 
 
-on : String -> Decoder msg -> Attribute attrs msg
-on =
+on : (attrs -> msg) -> String -> Decoder msg -> Attribute attrs msg
+on _ =
     Html.Typed.Internal.on
 
 
-onWithOptions : String -> Html.Events.Options -> Decoder msg -> Attribute attrs msg
-onWithOptions =
+onWithOptions : (attrs -> msg) -> String -> Html.Events.Options -> Decoder msg -> Attribute attrs msg
+onWithOptions _ =
     Html.Typed.Internal.onWithOptions
