5370eae4d23df9288a19023fd35f87828abf2ec4
Jasper Woudenberg <mail@jasperwoudenberg.com>
2018-02-10 09:32:35 +0100

    Move types back into Html.Typed

--- src/Html/Typed.elm
+++ src/Html/Typed.elm
@@ -1,12 +1,391 @@
-module Html.Typed exposing (Attribute, Html, a, abbr, add, address, article, aside, audio, b, bdi, bdo, body, br, button, canvas, caption, cite, code, col, colgroup, datalist, dd, debug, del, detail, dfn, div, dl, em, embed, fieldset, figcaption, figure, footer, form, fromRaw, h1, h2, h3, h4, h5, h6, header, hr, i, iframe, img, input, ins, kbd, keygen, label, legend, li, list, main_, map, mark, math, menu, menuitem, meter, name, nav, object, ol, optgroup, option, output, p, param, pre, progress, q, rp, rt, ruby, s, samp, section, select, small, source, span, strong, sub, summary, sup, table, tbody, td, text, textarea, tfoot, th, thead, time, toRaw, tr, track, u, ul, var, video, wbr, within)
+module Html.Typed exposing (A, Abbr, Address, Article, Aside, Attribute, Audio, B, Bdi, Bdo, Blockquote, Body, Br, Button, Canvas, Caption, Cite, Code, Col, Colgroup, Datalist, Dd, Del, Detail, Dfn, Dl, Em, Embed, Fieldset, Figcaption, Figure, Footer, Form, H1, H2, H3, H4, H5, H6, Header, Hr, Html, I, Iframe, Img, Input, Ins, Kbd, Keygen, Label, Legend, Li, Main, Mark, Math, Menu, Menuitem, Meter, Nav, Object, Ol, Optgroup, Option, Output, P, Param, Pre, Progress, Q, Rp, Rt, Ruby, S, Samp, Section, Select, Small, Source, Strong, Sub, Summary, Sup, Table, Tbody, Td, Textarea, Tfoot, Th, Thead, Time, Tr, Track, U, Ul, Var, Video, Wbr, a, abbr, add, address, article, aside, audio, b, bdi, bdo, body, br, button, canvas, caption, cite, code, col, colgroup, datalist, dd, debug, del, detail, dfn, div, dl, em, embed, fieldset, figcaption, figure, footer, form, fromRaw, h1, h2, h3, h4, h5, h6, header, hr, i, iframe, img, input, ins, kbd, keygen, label, legend, li, list, main_, map, mark, math, menu, menuitem, meter, name, nav, object, ol, optgroup, option, output, p, param, pre, progress, q, rp, rt, ruby, s, samp, section, select, small, source, span, strong, sub, summary, sup, table, tbody, td, text, textarea, tfoot, th, thead, time, toRaw, tr, track, u, ul, var, video, wbr, within)
 
 import Html
 import Html.Typed.Internal exposing (SubAttribute, mapAttr, mkAttr, toSubAttr)
-import Html.Typed.Types exposing (..)
 
 
 type alias Attribute attrs msg =
     Html.Typed.Internal.Attribute attrs msg
+
+
+type H1 attrs child
+    = H1Type Never
+
+
+type H2 attrs child
+    = H2Type Never
+
+
+type H3 attrs child
+    = H3Type Never
+
+
+type H4 attrs child
+    = H4Type Never
+
+
+type H5 attrs child
+    = H5Type Never
+
+
+type H6 attrs child
+    = H6Type Never
+
+
+type Section attrs child
+    = SectionType Never
+
+
+type P attrs child
+    = PType Never
+
+
+type Hr attrs child
+    = HrType Never
+
+
+type Pre attrs child
+    = PreType Never
+
+
+type Blockquote attrs child
+    = BlockquoteType Never
+
+
+type A attrs child
+    = AType Never
+
+
+type Code attrs child
+    = CodeType Never
+
+
+type Em attrs child
+    = EmType Never
+
+
+type Strong attrs child
+    = StrongType Never
+
+
+type I attrs child
+    = IType Never
+
+
+type B attrs child
+    = BType Never
+
+
+type U attrs child
+    = UType Never
+
+
+type Sub attrs child
+    = SubType Never
+
+
+type Sup attrs child
+    = SupType Never
+
+
+type Br attrs child
+    = BrType Never
+
+
+type Ol attrs child
+    = OlType Never
+
+
+type Ul attrs child
+    = UlType Never
+
+
+type Li attrs child
+    = LiType Never
+
+
+type Dl attrs child
+    = DlType Never
+
+
+type Dd attrs child
+    = DdType Never
+
+
+type Img attrs child
+    = ImgType Never
+
+
+type Iframe attrs child
+    = IframeType Never
+
+
+type Canvas attrs child
+    = CanvasType Never
+
+
+type Math attrs child
+    = MathType Never
+
+
+type Form attrs child
+    = FormType Never
+
+
+type Input attrs child
+    = InputType Never
+
+
+type Textarea attrs child
+    = TextareaType Never
+
+
+type Button attrs child
+    = ButtonType Never
+
+
+type Select attrs child
+    = SelectType Never
+
+
+type Option attrs child
+    = OptionType Never
+
+
+type Nav attrs child
+    = NavType Never
+
+
+type Article attrs child
+    = ArticleType Never
+
+
+type Aside attrs child
+    = AsideType Never
+
+
+type Header attrs child
+    = HeaderType Never
+
+
+type Footer attrs child
+    = FooterType Never
+
+
+type Address attrs child
+    = AddressType Never
+
+
+type Main attrs child
+    = MainType Never
+
+
+type Body attrs child
+    = BodyType Never
+
+
+type Figure attrs child
+    = FigureType Never
+
+
+type Figcaption attrs child
+    = FigcaptionType Never
+
+
+type Table attrs child
+    = TableType Never
+
+
+type Caption attrs child
+    = CaptionType Never
+
+
+type Colgroup attrs child
+    = ColgroupType Never
+
+
+type Col attrs child
+    = ColType Never
+
+
+type Tbody attrs child
+    = TbodyType Never
+
+
+type Thead attrs child
+    = TheadType Never
+
+
+type Tfoot attrs child
+    = TfootType Never
+
+
+type Tr attrs child
+    = TrType Never
+
+
+type Td attrs child
+    = TdType Never
+
+
+type Th attrs child
+    = ThType Never
+
+
+type Fieldset attrs child
+    = FieldsetType Never
+
+
+type Legend attrs child
+    = LegendType Never
+
+
+type Label attrs child
+    = LabelType Never
+
+
+type Datalist attrs child
+    = DatalistType Never
+
+
+type Optgroup attrs child
+    = OptgroupType Never
+
+
+type Keygen attrs child
+    = KeygenType Never
+
+
+type Output attrs child
+    = OutputType Never
+
+
+type Progress attrs child
+    = ProgressType Never
+
+
+type Meter attrs child
+    = MeterType Never
+
+
+type Audio attrs child
+    = AudioType Never
+
+
+type Video attrs child
+    = VideoType Never
+
+
+type Source attrs child
+    = SourceType Never
+
+
+type Track attrs child
+    = TrackType Never
+
+
+type Embed attrs child
+    = EmbedType Never
+
+
+type Object attrs child
+    = ObjectType Never
+
+
+type Param attrs child
+    = ParamType Never
+
+
+type Ins attrs child
+    = InsType Never
+
+
+type Del attrs child
+    = DelType Never
+
+
+type Small attrs child
+    = SmallType Never
+
+
+type Cite attrs child
+    = CiteType Never
+
+
+type Dfn attrs child
+    = DfnType Never
+
+
+type Abbr attrs child
+    = AbbrType Never
+
+
+type Time attrs child
+    = TimeType Never
+
+
+type Var attrs child
+    = VarType Never
+
+
+type Samp attrs child
+    = SampType Never
+
+
+type Kbd attrs child
+    = KbdType Never
+
+
+type S attrs child
+    = SType Never
+
+
+type Q attrs child
+    = QType Never
+
+
+type Mark attrs child
+    = MarkType Never
+
+
+type Ruby attrs child
+    = RubyType Never
+
+
+type Rt attrs child
+    = RtType Never
+
+
+type Rp attrs child
+    = RpType Never
+
+
+type Bdi attrs child
+    = BdiType Never
+
+
+type Bdo attrs child
+    = BdoType Never
+
+
+type Wbr attrs child
+    = WbrType Never
+
+
+type Detail attrs child
+    = DetailType Never
+
+
+type Summary attrs child
+    = SummaryType Never
+
+
+type Menuitem attrs child
+    = MenuItemType Never
+
+
+type Menu attrs child
+    = MenuType Never
 
 
 
@@ -155,7 +534,7 @@ u attrs child =
     Html <| Node "u" (List.map toSubAttr attrs) (toSubHtml child)
 
 
-sub : List (Attribute attrs msg) -> Html tipe msg -> Html (Html.Typed.Types.Sub attrs tipe) msg
+sub : List (Attribute attrs msg) -> Html tipe msg -> Html (Sub attrs tipe) msg
 sub attrs child =
     Html <| Node "sub" (List.map toSubAttr attrs) (toSubHtml child)
 
